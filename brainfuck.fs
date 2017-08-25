e5825c4e22895a8bb1a2e3531880d4fe24ccbc95
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-08-25 16:26:09 +0200

    adds working brainfuck interpreter in F#

--- brainfuck.fs
+++ brainfuck.fs
@@ -1,34 +1,57 @@
 open System
 
-module BrainFuck =
+module brainfuck =
 
-    let rec run (code : string, data : char[], iptr : int, dptr : int, count : int) =
-        match code.[iptr] with
-        | '>' -> 
-            run(code, data, iptr + 1, dptr + 1, count)
-        | '<' -> 
-            run(code, data, iptr + 1, dptr - 1, count)
-        | '+' ->
-            Array.set data dptr (char(int(data.[dptr]) + 1))
-            run(code, data, iptr + 1, dptr, count)
-        | '-' -> 
-            Array.set data dptr (char(int(data.[dptr]) - 1))
-            run(code, data, iptr + 1, dptr, count)
-        | '.' -> 
-            printfn "%c" data.[dptr]
-            run(code, data, iptr + 1, dptr, count)
-        | ',' -> 
-            printfn ","
-        | '[' -> 
-            printfn "["
-        | ']' -> 
-            printfn "]"
-        | _ -> 
-            run(code, data, iptr + 1, dptr, count)
+    type BrainFuck = {
+        code : string;
+        data : int[];
+    }
+
+    let rec retrieve_left (code : string, iptr : int, count : int) =
+        if count > 0 then
+            if code.[iptr] = ']' then retrieve_left(code, iptr - 1, count + 1)
+            elif code.[iptr] = '[' then retrieve_left(code, iptr - 1, count - 1)
+            else retrieve_left(code, iptr - 1, count)
+        else
+            iptr + 1
+
+    let rec retrieve_right (code : string, iptr : int, count : int) =
+        if count > 0 then
+            if code.[iptr] = '[' then retrieve_right(code, iptr + 1, count + 1)
+            elif code.[iptr] = ']' then retrieve_right(code, iptr + 1, count - 1)
+            else retrieve_right(code, iptr + 1, count)
+        else
+            iptr
+
+    let rec run (bf : BrainFuck, iptr : int, dptr : int) =
+        if String.length bf.code > iptr then
+            match bf.code.[iptr] with
+            | '>' -> 
+                run(bf, iptr + 1, dptr + 1)
+            | '<' -> 
+                run(bf, iptr + 1, dptr - 1)
+            | '+' ->
+                Array.set bf.data dptr (bf.data.[dptr] + 1)
+                run(bf, iptr + 1, dptr)
+            | '-' -> 
+                Array.set bf.data dptr (bf.data.[dptr] - 1)
+                run(bf, iptr + 1, dptr)
+            | '.' -> 
+                printf "%c" (char(bf.data.[dptr]))
+                run(bf, iptr + 1, dptr)
+            | ',' -> 
+                Array.set bf.data dptr (System.Console.Read())
+            | '[' -> 
+                if bf.data.[dptr] = 0 then run(bf, retrieve_right(bf.code, iptr, 1), dptr)
+                else run(bf, iptr + 1, dptr)
+            | ']' ->
+                if bf.data.[dptr] > 0 then run(bf, retrieve_left(bf.code, iptr - 1, 1), dptr)
+                else run(bf, iptr + 1, dptr)
+            | _ -> 
+                run(bf, iptr + 1, dptr)
 
 [<EntryPoint>]
 let main argv = 
-    let data : char[] = Array.init 30000 (fun x -> char(0))
-    let code : string = System.IO.File.ReadAllText(argv.[0])
-    BrainFuck.run(code, data, 0, 1, 1)
+    let bf : brainfuck.BrainFuck = {code = System.IO.File.ReadAllText argv.[0]; data = Array.init 30000 (fun x -> 0);}
+    brainfuck.run(bf, 0, 0)
     0;
