bf4c4910056a5f4511ddc7713e1ecbdd72c5307d
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-08-26 15:33:04 +0200

    adds bash brainfuck interpreter

--- brainfuck.bash
+++ brainfuck.bash
@@ -8,16 +8,33 @@ dptr=0
 iptr=0
 
 while [[ $iptr -lt $((len + 1)) ]]; do
+    count=1
     case ${code:$iptr:1} in
         '>') dptr=$((dptr+1));;
         '<') dptr=$((dptr-1));;
         '+') data[$dptr]=$((data[$dptr] + 1));;
         '-') data[$dptr]=$((data[$dptr] - 1));;
         '.') printf "\x` printf %x ${data[$dptr]}`";;
-        ',') read -n1 s;
-             data[$dptr]=`printf "%d" \'$s` ;;
-        '[') ;;
-        ']');;
+        ',') read -n1 s; data[$dptr]=`printf "%d" \'$s` ;;
+        '[') 
+            if [[ data[$dptr] == 0 ]]; then
+                iptr=$((iptr+1))
+                while [[ $count -gt 0 ]]; do
+                    if [[ ${code:$iptr:1} == '[' ]]; then count=$((count+1)); fi
+                    if [[ ${code:$iptr:1} == ']' ]]; then count=$((count-1)); fi
+                    iptr=$((iptr+1))
+                done
+                iptr=$((iptr-1))
+            fi;;
+        ']')
+            if [[ data[$dptr] -ne 0 ]]; then
+                iptr=$((iptr-1))
+                while [[ $count -gt 0 ]]; do
+                    if [[ ${code:$iptr:1} == ']' ]]; then count=$((count+1)); fi
+                    if [[ ${code:$iptr:1} == '[' ]]; then count=$((count-1)); fi
+                    iptr=$((iptr-1))
+                done
+            fi;;
     esac
     iptr=$((iptr+1))
 done
